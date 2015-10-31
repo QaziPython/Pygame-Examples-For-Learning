1b598e72295b0c7e36ad811ff4a7c75c3d30d0e2
rafeh01 <rafehqazi1@gmail.com>
2015-10-31 11:37:02 -0500

    vim-ified level1.py. Movement is now with 'hjkl'. Refactored the code.

--- 3 Level Hungry Snake/level1.py
+++ 3 Level Hungry Snake/level1.py
@@ -1,19 +1,19 @@
-#!/Users/Rafeh/anaconda/envs/python2/bin/python2.7
+#!/Users/Rafeh/anaconda/envs/python2/bin python
 ############################################################################
 # Purpose : A very small, basic and my first game
 # Usages : Learning purpose
-# Start date : 12/04/2011
-# End date : 2/05/2011
+# Start date : 10/31/2015
+# End date : In progress
 # Author : Ankur Aggarwal
+# Editor : Rafeh Qazi
 # License : GNU GPL v3 http://www.gnu.org/licenses/gpl.html
 # Version : 0.0.2
 # Modification history : level1-Snake passage through the border
 ############################################################################
 
 
-
 import pygame
-from  pygame.locals import *
+from pygame.locals import *
 from sys import exit
 from random import randint
 
@@ -22,150 +22,123 @@ pygame.init()
 COUNTER=0
 
 def main():
- while True:
-  b=[]
-#update function used for incrementing the COUNTER
-  def update():
-   global COUNTER
-   COUNTER=(COUNTER+1)%7
-# blast function used for creating the blast through sprites on collision 
-  def blast(w,h):
-   image=pygame.image.load("explosed-sprite.png").convert_alpha()
-   width,height=image.get_size()
-   for i in xrange(int(width/w)):
-    b.append(image.subsurface((i*w,0,w,h)))
-   #print a
-  up=1
-  down=2
-  right=3
-  left=4
-  step=20
-  block=[20,20]
-  x=randint(1,20)
-  y=randint(2,22)
-  applexy=[]
-  snakexy=[int(x*20),int(y*20)]
-  snakelist=[[x*20,y*20],[(x-20)*20,(y*20)]]
-  apple=0
-  dead=0
-  grow=0
-  direction=right
-  score=0
-  start=0
-  screen=pygame.display.set_mode((640,480),0,24)
-  clock=pygame.time.Clock()
+    COUNTER = 0
+    STATE = []
+    UP = 1
+    DOWN = 2
+    RIGHT = 3
+    LEFT = 4
+    STEP = 20
+    BLOCK = [20, 20]
 
-#game loop
-  while not dead:
-   pressed=pygame.key.get_pressed()
-   for i in pygame.event.get():
-    if i.type==QUIT or pressed[K_q]:
-     exit()
-   if pressed[K_h] and direction!=right:
-     direction=left
-   elif pressed[K_l] and direction!=left:
-      direction=right
-   elif pressed[K_k] and direction!=down:
-      direction=up
-   elif pressed[K_j] and direction!=up:
-      direction=down
-   if direction==right:
-    snakexy[0]=snakexy[0]+step
-    if snakexy[0]>=640:
-     snakexy[0]=0
+    # random coordinates x, y used for initializing snake
+    X = randint(1, 20)
+    Y = randint(2, 22)
+    SNAKE = [int(X*20), int(Y*20)]
+    SNAKELIST = [[X*20, Y*20], [(X-20)*20, (Y*20)]]
 
-   elif direction==left:
-    snakexy[0]=snakexy[0]-step
-    if snakexy[0]<0:
-     snakexy[0]=620
+    # initialize random apple coordinate
+    APPLE = (int(randint(1, 31)*STEP), int(randint(2, 22)*STEP))
 
-   elif direction==up:
-    snakexy[1]=snakexy[1]-step
-    if snakexy[1]<0:
-     snakexy[1]=460
-   elif direction==down:
-    snakexy[1]=snakexy[1]+step
-    if snakexy[1]>=480:
-     snakexy[1]=0
+    DEAD = 0
+    DIRECTION = RIGHT
+    SCORE = 0
+    FONT1 = pygame.font.SysFont("Arial", 30)
+    SCREEN = pygame.display.set_mode((640, 480), 0, 24)
+    CLOCK = pygame.time.Clock()
 
-   if snakelist.count(snakexy)>0:
-    dead=1
-   if apple==0:
-    x1=randint(1,31)
-    y1=randint(2,22)
-    applexy=[int(x1*step),int(y1*step)]
-    apple=1
+    # blast function used for creating the blast through sprites on collision
+    def blast(w, h):
+        image = pygame.image.load("explosed-sprite.png").convert_alpha()
+        width, height = image.get_size()
+        for i in xrange(int(width/w)):
+            STATE.append(image.subsurface((i*w, 0, w, h)))
 
-   snakelist.insert(0,list(snakexy))
-   if snakexy[0]==applexy[0] and snakexy[1]==applexy[1]:
-    apple=0
-    score=score+1
-   else:
-    snakelist.pop()
-#display on the screen
-   screen.fill((0,0,0))
-   scr=pygame.font.SysFont("Arial",20)
-   text4=scr.render("Score : %d"%score,True,(0,255,0))
-   screen.blit(text4,(500,10))
-   s1=pygame.font.SysFont("Arial",30)
-   screen.blit(s1.render("phajanngggg",True,(0,255,0)),(50,250))
-   pygame.draw.rect(screen,(255,0,0),Rect(applexy,block),0)
-   for i in snakelist:
-    pygame.draw.rect(screen,(0,255,0),Rect(i,block))
-   pygame.display.flip()
-   clock.tick(15)
+    # game loop
+    while not DEAD:
+        pressed = pygame.key.get_pressed()
+        # pygame.event.get() returns a dict of current state of the game
+        for event in pygame.event.get():
+            if event.type == QUIT or pressed[K_q]:
+                exit()
+        if pressed[K_h] and DIRECTION != RIGHT:
+            DIRECTION = LEFT
+        elif pressed[K_l] and DIRECTION != LEFT:
+            DIRECTION = RIGHT
+        elif pressed[K_k] and DIRECTION != DOWN:
+            DIRECTION = UP
+        elif pressed[K_j] and DIRECTION != UP:
+            DIRECTION = DOWN
+        if DIRECTION == RIGHT:
+            SNAKE[0] = SNAKE[0]+STEP
+            if SNAKE[0] >= 640:
+                SNAKE[0] = 0
 
+        elif DIRECTION == LEFT:
+            SNAKE[0] = SNAKE[0]-STEP
+            if SNAKE[0] < 0:
+                SNAKE[0] = 620
 
-  if dead==1:
-   blast(20,20)
-   for i in xrange(7):
-    screen.blit(b[COUNTER],(snakexy[0],snakexy[1]))
-    update()
-    pygame.display.update()
-    clock.tick(10)
-#game over
-   screen.fill((0,0,0))
-   over=pygame.font.SysFont("Arial",40)
-   text5=over.render("GAME OVER",True,(0,255,0))
-   s1=pygame.font.SysFont("Arial",30)
-   s2=pygame.font.SysFont("Arial",30)
-   screen.blit(text5,(50,50))
-   screen.blit(text4,(200,200))
-   screen.blit(s1.render("Press s To Play Again",True,(0,255,0)),(50,250))
-   screen.blit(s2.render("Press l For Level Selection ",True,(0,255,0)),(50,300))
-   pygame.display.flip()
-   while True:
-    for i in pygame.event.get():
-     if i.type==QUIT:
-      exit()
-    pressed=pygame.key.get_pressed()
-    if pressed[K_q]:
-     exit()
-    if pressed[K_s]:
-     main()
-    if pressed[K_l]:
-     break
-   break
+        elif DIRECTION == UP:
+            SNAKE[1] = SNAKE[1]-STEP
+            if SNAKE[1] < 0:
+                SNAKE[1] = 460
+        elif DIRECTION == DOWN:
+            SNAKE[1] = SNAKE[1]+STEP
+            if SNAKE[1] >= 480:
+                SNAKE[1] = 0
+
+        if SNAKELIST.count(SNAKE) > 0:
+            DEAD = 1
+
+        SNAKELIST.insert(0, list(SNAKE))
+        if SNAKE[0] == APPLE[0] and SNAKE[1] == APPLE[1]:
+            APPLE = (int(randint(1, 31)*STEP), int(randint(2, 22)*STEP))
+            SCORE = SCORE+1
+        else:
+            SNAKELIST.pop()
+        # display on the screen
+        SCREEN.fill((0, 0, 0))
+        scr = pygame.font.SysFont("Arial", 20)
+        display_score = scr.render("Score : %d" % SCORE, True, (0, 255, 0))
+        SCREEN.blit(display_score, (500, 10))
+        FONT1 = pygame.font.SysFont("Arial", 30)
+        SCREEN.blit(FONT1.render("phajanngggg", True, (0, 255, 0)), (50, 250))
+        pygame.draw.rect(SCREEN, (255, 0, 0), Rect(APPLE, BLOCK), 0)
+        for i in SNAKELIST:
+            pygame.draw.rect(SCREEN, (0, 255, 0), Rect(i, BLOCK))
+        pygame.display.flip()
+        CLOCK.tick(15)
+
+    if DEAD == 1:
+        blast(20, 20)
+        for _ in xrange(7):
+            SCREEN.blit(STATE[COUNTER], (SNAKE[0], SNAKE[1]))
+            COUNTER = (COUNTER+1) % 7
+            pygame.display.update()
+            CLOCK.tick(10)
+        # game over
+        SCREEN.fill((0, 0, 0))
+        over = pygame.font.SysFont("Arial", 40)
+        display_game_over = over.render("GAME OVER", True, (0, 255, 0))
+        SCREEN.blit(display_game_over, (50, 50))
+        SCREEN.blit(display_score, (200, 200))
+        SCREEN.blit(FONT1.render("Press P To Play Again", True,
+                                 (0, 255, 0)), (50, 250))
+        SCREEN.blit(FONT1.render("Press Q to Quit", True,
+                                 (0, 255, 0)), (50, 350))
+        pygame.display.flip()
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+            pressed = pygame.key.get_pressed()
+            # quit on Q
+            if pressed[K_q]:
+                exit()
+            if pressed[K_p]:
+                main()
 
 
-
-if __name__=='__main__':
- main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
